cb8013195688a7098dfb19703fdfc52e5d163ae4
NandaYogeshwar <nanda@yogeshwar.de>
2019-04-16 13:09:19 +0200

    moved initialization to body onload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-$(function(){
-    addDummyMessages();
-    listChannels(compareNew); 
-    loadEmojis();
-    console.log("App is initialized");
-    setInterval(function(){ 
-        console.log("Updating message elements...");
-        for(var i=0; i<channels.length; i++){
-            $.each(channels[i].messages, function(index, value) {
-                value.update();
-            }); 
-        }
-     }, 10000);
-});
+// $(function(){
+//     addDummyMessages();
+//     listChannels(compareNew); 
+//     loadEmojis();
+//     console.log("App is initialized");
+//     setInterval(function(){ 
+//         console.log("Updating message elements...");
+//         for(var i=0; i<channels.length; i++){
+//             $.each(channels[i].messages, function(index, value) {
+//                 value.update();
+//             }); 
+//         }
+//      }, 10000);
+// });
 
 /** create global variable */
 var currentChannel = sevencontinents;
